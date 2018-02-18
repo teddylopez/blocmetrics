03ac4ceae155501a54756be55e163525d78e1999
teddylopez <tedmlopez@gmail.com>
2018-02-18 12:11:07 -0500

    Nav styling.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,30 @@
-#require 'faker'
+require 'faker'
 
 # Create a test user
-#test_user = User.new(
-# username:     'test',
-# email:    'test@blocmetrics.com',
-# password: 'password',
-#)
-#test_user.save!
+test_user = User.new(
+ username:     'test',
+ email:    'test@tedmetrics.com',
+ password: 'password',
+)
+test_user.save!
 
 # Create registered applications
-#5.times do
-# RegisteredApp.create!(
-#   user: test_user,
-#   name: Faker::App.name,
-#   url:  Faker::Internet.domain_name
-# )
-#end
-#apps = RegisteredApp.all
+5.times do
+ RegisteredApp.create!(
+   user: test_user,
+   name: Faker::App.name,
+   url:  Faker::Internet.domain_name
+ )
+end
+apps = RegisteredApp.all
 
-#20.times do
-#Event.create!(
-#  name: Faker::Lorem.sentence,
-#  registered_app: RegisteredApp.first
-#  )
-#end
+20.times do
+Event.create!(
+  name: Faker::Lorem.sentence,
+  registered_app: RegisteredApp.first
+  )
+end
 
-#puts "Seed finished"
-#puts "#{RegisteredApp.count} apps created"
-#puts "#{Event.count} events created"
+puts "Seed finished"
+puts "#{RegisteredApp.count} apps created"
+puts "#{Event.count} events created"
